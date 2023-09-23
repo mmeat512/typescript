83f8995533bcf1e00212809c07f2e0d166bc30f9
mmeat512 <wndus0958@naver.com>
2023-09-23 12:28:41 +0900

    [#11] modal 구현

--- beginner-project/src/ts/modal.ts
+++ beginner-project/src/ts/modal.ts
@@ -1,3 +1,15 @@
-function fModal() {}
+function fModal() {
+  const toggleBtn =
+    document.querySelector<HTMLButtonElement>('.modal .toggle-btn');
+  const modalEl = document.querySelector<HTMLDivElement>('.modal .modal-inner');
+  const closeBtn =
+    document.querySelector<HTMLButtonElement>('.modal .close-btn');
+  toggleBtn?.addEventListener('click', () => {
+    modalEl!.classList.add('active');
+  });
+  closeBtn?.addEventListener('click', () => {
+    modalEl?.classList.remove('active');
+  });
+}
 
 export default fModal;
